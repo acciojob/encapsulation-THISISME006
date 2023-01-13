f673abcc4c05ed58ced94abf35e118868aa43e70
VaibhavBhatt <vaibhavbhatt23@gmail.com>
2023-01-13 17:25:37 +0530

    Sol 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,10 @@ package com.driver;
 public class Main {
   public static void main(String[] args){
       RWOnly obj = new RWOnly();
-      obj.name="Vaivv";
+      //obj.name="Vaivv";
       System.out.print(obj.name);
 //      java: name has private access in com.driver.RWOnly
-      obj.getName();
       obj.setName("John");
+      obj.getName();
   }
 }
